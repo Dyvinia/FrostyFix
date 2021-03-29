30a7a42af40f6b8f1d36888aebbcecf9ad850c1a
Diana Gunasena <dulana57@gmail.com>
2021-03-29 16:47:29 -0400

    Remove uneeded usings

--- AboutWindow.xaml.cs
+++ AboutWindow.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FrostyFix2 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for AboutWindow.xaml
     /// </summary>
     public partial class AboutWindow : Window {
         public AboutWindow() {
