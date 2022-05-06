14d61411c39b14939b1cbe56c7607c013927230a
Dyvine57 <dulana57@gmail.com>
2022-05-05 21:28:36 -0400

    'handle' unhandled exceptions

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,11 +5,17 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FrostyFix4 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            App.Current.Shutdown();
+        }
     }
 }
