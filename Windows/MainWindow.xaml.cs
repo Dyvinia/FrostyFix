0373f821ba9f8d4365fcd0c65ad51684d41d8325
Dyvine57 <dulana57@gmail.com>
2022-05-21 15:52:17 -0400

    fix

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -211,7 +211,7 @@ namespace FrostyFix4 {
                             Settings.Default.backgroundThread = false;
                             Settings.Default.Save();
 
-                            string title = "FrostyFix 4";
+                            string title = this.Title;
                             string message = "Background thread has encountered an error and has been disabled:\n" + ex.Message;
                             if (ex.InnerException != null)
                                 message += Environment.NewLine + Environment.NewLine + ex.InnerException;
