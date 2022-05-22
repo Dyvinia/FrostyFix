76dd54b73525c8dd81e92f73093a2e93507bd1dd
Dyvine57 <dulana57@gmail.com>
2022-05-21 21:08:07 -0400

    tidy up code

--- Dialogs/ExceptionDialog.xaml.cs
+++ Dialogs/ExceptionDialog.xaml.cs
@@ -25,37 +25,31 @@ namespace FrostyFix4.Dialogs {
             Window mainWindow = Application.Current.MainWindow;
             Icon = mainWindow.Icon;
 
+            // Calculate Window Height
             int headerHeight = 30;
             int height = 220;
-
             if (!isCrash) {
                 headerHeight = 5;
                 height -= 25;
                 HeaderText.Visibility = Visibility.Collapsed;
             }
-
-            string message = ex.Message;
-
-            if (messagePrefix != null) 
-                message = messagePrefix + Environment.NewLine + message;
-
-            if (ex.InnerException != null)
-                message += Environment.NewLine + Environment.NewLine + ex.InnerException;
-            else height -= 50;
-
-            ExceptionText.Text = message;
-
             Header.Height = new GridLength(headerHeight);
             Height = height + headerHeight;
 
-            CopyButton.Click += (s, e) => Clipboard.SetText(message);
+            // Create exception message
+            string message = ex.Message;
+            if (messagePrefix != null) 
+                message = messagePrefix + Environment.NewLine + message;
+            if (ex.InnerException != null)
+                message += Environment.NewLine + Environment.NewLine + ex.InnerException;
+            ExceptionText.Text = message;
 
             if (isCrash) CloseButton.Click += (s, e) => Environment.Exit(0);
             else CloseButton.Click += (s, e) => this.Close();
+            CopyButton.Click += (s, e) => Clipboard.SetText(message);
 
             SystemSounds.Hand.Play();
         }
-
 
         public static void Show(Exception ex, string title, bool isCrash = false, string messagePrefix = null) {
             Application.Current.Dispatcher.Invoke(() => {
